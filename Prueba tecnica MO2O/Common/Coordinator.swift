5e8a92a7fccc5057ca45e03c8880a510adf311ba
Wanaldino Antimonio <carlosmm31093@gmail.com>
2020-10-21 11:13:58 +0200

    Added coordinators for routing

---
+++ Prueba tecnica MO2O/Common/Coordinator.swift
@@ -0,0 +1,12 @@
+//
+//  Coordinator.swift
+//  Prueba tecnica MO2O
+//
+//  Created by Wanaldino Antimonio on 21/10/2020.
+//
+
+import Foundation
+
+protocol Coordinator: class {
+    func start()
+}
---
+++ Prueba tecnica MO2O/MainCoordinator.swift
@@ -0,0 +1,20 @@
+//
+//  MainCoordinator.swift
+//  Prueba tecnica MO2O
+//
+//  Created by Wanaldino Antimonio on 21/10/2020.
+//
+
+import UIKit
+
+class MainCoordinator: Coordinator {
+    let navigator: UINavigationController
+    
+    init(navigator: UINavigationController) {
+        self.navigator = navigator
+    }
+
+    func start() {
+        
+    }
+}
