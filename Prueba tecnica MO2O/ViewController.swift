623890ab23c3a29f4fb83ce5ccb6eb5ebb75a38c
Wanaldino Antimonio <carlosmm31093@gmail.com>
2020-10-21 10:58:58 +0200

    Initial Commit

---
+++ Prueba tecnica MO2O/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prueba tecnica MO2O
+//
+//  Created by Wanaldino Antimonio on 21/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
