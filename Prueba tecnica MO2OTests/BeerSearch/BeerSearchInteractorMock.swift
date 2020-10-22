f338b7068a649237b9d618d10d6cd05ec9c7a814
Wanaldino Antimonio <carlosmm31093@gmail.com>
2020-10-22 17:01:33 +0200

    Added some tests

---
+++ Prueba tecnica MO2OTests/BeerSearch/BeerSearchInteractorMock.swift
@@ -0,0 +1,8 @@
+//
+//  BeerSearchInteractorMock.swift
+//  Prueba tecnica MO2OTests
+//
+//  Created by Wanaldino Antimonio on 22/10/2020.
+//
+
+import Foundation
---
+++ Prueba tecnica MO2OTests/BeerSearch/BeerSearchPresenterTest.swift
@@ -0,0 +1,8 @@
+//
+//  BeerSearchPresenterTest.swift
+//  Prueba tecnica MO2OTests
+//
+//  Created by Wanaldino Antimonio on 22/10/2020.
+//
+
+import Foundation
